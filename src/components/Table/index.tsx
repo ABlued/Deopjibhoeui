a04ccd421a11224329ae4030d153bd4ed6889c26
ABlued <skwodhkswkd@naver.com>
2024-08-27 22:08:29 +0900

    feat: Table 구현

---
+++ src/components/Table/index.tsx
@@ -0,0 +1,85 @@
+import * as React from 'react';
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable
+} from '@tanstack/react-table';
+
+function Table<T>({
+  rowData,
+  columns
+}: {
+  rowData: T[];
+  columns: ColumnDef<T, any>[];
+}) {
+  const [data, _setData] = React.useState(() => [...rowData]);
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel()
+  });
+
+  return (
+    <div className="relative overflow-x-auto">
+      <table className="w-full border-[1px solid lightgray] text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-[#F1F6F7] dark:bg-gray-700 dark:text-gray-400">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th
+                  key={header.id}
+                  className="px-6 py-4 border-[lightgray] border-b-[1px] border-r-[1px]"
+                >
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody className="border-b-[1px solid lightgray]">
+          {table.getRowModel().rows.map((row) => (
+            <tr
+              key={row.id}
+              className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+            >
+              {row.getVisibleCells().map((cell) => (
+                <td
+                  key={cell.id}
+                  className="px-6 py-4 border-[lightgray] border-b-[1px] border-r-[1px]"
+                >
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          {table.getFooterGroups().map((footerGroup) => (
+            <tr key={footerGroup.id}>
+              {footerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.footer,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+      <div className="h-4" />
+    </div>
+  );
+}
+
+export default Table;
