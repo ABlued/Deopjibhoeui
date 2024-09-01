4c773f8737d1838c7ae3250536c79a922b46878c
ABlued <skwodhkswkd@naver.com>
2024-09-01 22:11:26 +0900

    fix: 정산 제목이 없어도 validator가 에러를 일으키지 않는 버그 수정

--- src/core/utils/validator/rules/emptyTextRule.ts
+++ src/core/utils/validator/rules/emptyTextRule.ts
@@ -2,6 +2,6 @@ import { ValidationResult, ValidationRule } from '../../types/validate';
 
 export class EmptyTextRule implements ValidationRule<string> {
   validate(target: string): ValidationResult {
-    return { isError: !(target.length > 0), message: '필수 항목입니다.' };
+    return { isError: target.length === 0, message: '필수 항목입니다.' };
   }
 }
