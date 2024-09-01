e05607e6f036f60273bd01bcbcc8f84829c0d149
ABlued <skwodhkswkd@naver.com>
2024-09-01 22:08:59 +0900

    feat: class-variance-authority Button에 적용

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,34 +1,77 @@
-import React from 'react';
-import { twMerge } from 'tailwind-merge';
+import { VariantProps, cva } from 'class-variance-authority';
+import React, { ButtonHTMLAttributes } from 'react';
+import { cn } from '../../core/utils/classname/cn';
 
-export interface ButtonProps {
+export interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof ButtonVariants> {
   text: string;
   disabled?: boolean;
   onClick: () => void;
   className?: string;
   fullWidth?: boolean;
+  startIcon?: () => JSX.Element;
 }
+
+const ButtonVariants = cva(
+  `
+text-white bg-primary-main hover:bg-primary-light focus:ring-4 focus:ring-blue-300 h-auto font-medium rounded-lg text-sm px-5 py-2.5 shadow-[4px_12px_20px_rgba(0,0,0,0.25)] dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800
+  `,
+  {
+    variants: {
+      variant: {
+        default: ' shadow-none active:scale-100',
+        grey: ' bg-slate-buttongrey ',
+        blue: ' bg-accent-blue'
+      },
+      size: {
+        default: '',
+        md: ' w-[6.875rem] h-[2.375rem] text-[1rem] rounded-md',
+        lg: 'w-[21.875rem] h-[7.5rem] text-[3rem] rounded-3xl',
+        wlg: 'w-[24rem] h-[5.25rem] text-[2rem]'
+      },
+      disabled: {
+        true: 'opacity-50 cursor-default pointer-events-none',
+        false: ''
+      },
+      fullWidth: {
+        true: 'w-full',
+        false: ''
+      }
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+      disabled: false,
+      fullWidth: false
+    }
+  }
+);
 
 function Button({
   text,
+  variant,
+  size,
   onClick,
   className,
   fullWidth,
-  disabled
+  disabled,
+  startIcon
 }: ButtonProps) {
   return (
-    <button
-      type="button"
-      className={twMerge(
-        'text-white bg-primary-main hover:bg-primary-light focus:ring-4 focus:ring-blue-300 h-[40px] font-medium rounded-lg text-sm px-5 py-2.5 shadow-[4px_12px_20px_rgba(0,0,0,0.25)] dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800',
-        fullWidth && 'w-full',
-        disabled && 'opacity-50 cursor-not-allowed',
-        className
-      )}
-      onClick={onClick}
-    >
-      {text}
-    </button>
+    <>
+      <button
+        type="button"
+        className={cn(
+          ButtonVariants({ variant, size, disabled, fullWidth }),
+          className
+        )}
+        onClick={onClick}
+      >
+        {startIcon && startIcon()}
+        {text}
+      </button>
+    </>
   );
 }
 
