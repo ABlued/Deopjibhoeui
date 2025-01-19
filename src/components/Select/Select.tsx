f81c2d59d0a21538ffbae58ca24f035061ba8f63
ABlued <skwodhkswkd@naver.com>
2025-01-19 21:13:09 +0900

    fix: Select의 Selected가 제대로 동작하지 않는 이슈 수정

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -7,7 +7,7 @@ type Item = {
 
 interface Props {
   items: Item[];
-  placeholder: string;
+  placeholder?: string;
   selected?: string;
   onChange?: (value: string) => void;
   className?: string;
@@ -17,23 +17,31 @@ function Select({ items, selected, onChange, placeholder, className }: Props) {
   return (
     <div>
       <form className="max-w-sm mx-auto">
-        <label htmlFor="underline_select" className="sr-only">
-          {placeholder}
-        </label>
+        {placeholder && (
+          <label htmlFor="underline_select" className="sr-only">
+            {placeholder}
+          </label>
+        )}
         <select
           id="underline_select"
           className="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
           onChange={(e) => {
             onChange?.(e.currentTarget.value);
           }}
-          defaultValue={''}
+          defaultValue={selected ?? ''}
         >
-          <option selected value={''}>
-            {placeholder}
-          </option>
+          {placeholder && (
+            <option selected value={''}>
+              {placeholder}
+            </option>
+          )}
           {items.sort().map((item) => {
             return (
-              <option key={item.value} value={item.value}>
+              <option
+                key={item.value}
+                value={item.value}
+                selected={selected === item.value}
+              >
                 {item.label}
               </option>
             );
