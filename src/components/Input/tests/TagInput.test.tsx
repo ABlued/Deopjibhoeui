73399a1b83bec1dcc514e7deae8e2ed6e714a680
ABlued <skwodhkswkd@naver.com>
2025-02-11 22:59:44 +0900

    test: TagInput 삭제 버튼 테스트 코드 추가 작성

--- src/components/Input/tests/TagInput.test.tsx
+++ src/components/Input/tests/TagInput.test.tsx
@@ -1,18 +1,23 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import TagInput from '../TagInput';
 import { InputTag } from '../../../core/hooks/types/useTagInput';
+
 const renderComponent = (tags: InputTag[]) => {
+  const addFn = jest.fn();
+  const deleteFn = jest.fn();
   render(
     <TagInput
-      addTags={jest.fn()}
-      removeTags={jest.fn()}
+      addTags={addFn}
+      removeTags={deleteFn}
       tags={tags}
       placeholder="test"
     />
   );
   const input = screen.getByPlaceholderText('test');
   return {
-    input
+    input,
+    addFn,
+    deleteFn
   };
 };
 
@@ -28,5 +33,16 @@ describe('When: input is "아이스크림" and Enter key is pressed', () => {
     const initTags = '아이스크림';
     renderComponent([initTags]);
     expect(screen.getByText(initTags)).toBeInTheDocument();
+  });
+  describe('When: remove button is clicked', () => {
+    test('Then: remove button should remove tag', () => {
+      const deleteIndex = 0;
+      const initTags = '아이스크림';
+      const { deleteFn } = renderComponent([initTags]);
+      const removeButton = screen.getByRole('button');
+      fireEvent.click(removeButton);
+      expect(deleteFn).toHaveBeenCalledTimes(1);
+      expect(deleteFn).toHaveBeenCalledWith(deleteIndex);
+    });
   });
 });
