945f86df6938083108df817fa749ec22950c3d69
ABlued <skwodhkswkd@naver.com>
2025-02-25 22:41:33 +0900

    feat: 홈 배경화면 parallax 효과 삭제 및 배경사진 반응형 적용

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,46 +1,43 @@
 import React from 'react';
 import { useNavigate } from 'react-router';
-import HomepageBackground from '../../assets/images/overviewBackground.png';
+import HomeRightBackground from '../../assets/images/homeRightBackground.png';
+import HomeLeftBackground from '../../assets/images/homeLeftBackground.png';
 import Button from '../../components/Button/Button';
 import { routePath } from '../../modules/router/routePath';
-import {
-  MouseParallaxContainer,
-  MouseParallaxChild
-} from 'react-parallax-mouse';
 function HomePage() {
   const navigate = useNavigate();
   return (
-    <MouseParallaxContainer globalFactorX={1} globalFactorY={1} resetOnLeave>
-      <div>
-        <MouseParallaxChild factorX={0.05} factorY={0.1}>
-          <img
-            src={HomepageBackground}
-            alt="background"
-            className="w-[105vw] max-w-[105vw] h-[100vh] absolute z-[-1] right-[-40px]"
-          />
-        </MouseParallaxChild>
-        <MouseParallaxChild factorX={0} factorY={0.05}>
-          <div className="flex flex-col items-center justify-center gap-[38px] w-[100vw] h-[100vh] text-center whitespace-pre">
-            <p className="text-[#000000] text-[48px] font-bold ">
-              {'덮집회의로\n 정산을 쉽고, 빠르게,\n 그리고 차분하게'}
-            </p>
-            <p className="text-[24px] font-[500]">
-              {
-                '덮집회의 웹서비스의 도움으로 정산의 고통을 줄이고,\n 친구들과 싸우는 걸 방지하여\n 부디 멋진 여행을 끝마쳐보도록 합시다.'
-              }
-            </p>
+    <div>
+      <img
+        src={HomeLeftBackground}
+        alt="background"
+        className="w-[30%] max-w-[30%] min-w-[200px] h-[100vh] absolute z-[-1] left-[-40px] opacity-50 sm:opacity-100"
+      />
+      <img
+        src={HomeRightBackground}
+        alt="background"
+        className="w-[30%] max-w-[30%] min-w-[200px] h-[100vh] absolute z-[-1] right-[-40px] opacity-50 sm:opacity-100"
+      />
 
-            <Button
-              text="시작하기"
-              onClick={() => {
-                navigate(routePath.setTitle);
-              }}
-              className={'bg-primary-dark w-[300px] text-[16px] font-semibold'}
-            />
-          </div>
-        </MouseParallaxChild>
+      <div className="flex flex-col items-center justify-center gap-[38px] w-[100vw] h-[100vh] text-center whitespace-pre">
+        <p className="text-[#000000] text-[48px] font-bold ">
+          {'덮집회의로\n 정산을 쉽고, 빠르게,\n 그리고 차분하게'}
+        </p>
+        <p className="text-[24px] font-[500]">
+          {
+            '덮집회의 웹서비스의 도움으로 정산의 고통을 줄이고,\n 친구들과 싸우는 걸 방지하여\n 부디 멋진 여행을 끝마쳐보도록 합시다.'
+          }
+        </p>
+
+        <Button
+          text="시작하기"
+          onClick={() => {
+            navigate(routePath.setTitle);
+          }}
+          className={'bg-primary-dark w-[300px] text-[16px] font-semibold'}
+        />
       </div>
-    </MouseParallaxContainer>
+    </div>
   );
 }
 
