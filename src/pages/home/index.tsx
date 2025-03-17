f7451093394bc92a6828c376395a4717b8e6972b
ABlued <skwodhkswkd@naver.com>
2025-03-17 23:38:53 +0900

    style: 홈 화면 문구 및 배경화면 반응형 구현

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -11,19 +11,19 @@ function HomePage() {
       <img
         src={HomeLeftBackground}
         alt="background"
-        className="w-[30%] max-w-[30%] min-w-[200px] h-[100vh] absolute z-[-1] left-[-40px] opacity-50 sm:opacity-100"
+        className="w-[30%] max-w-[30%] min-w-[200px] h-[100vh] absolute z-[-1] left-[-40px] hidden sm:inline"
       />
       <img
         src={HomeRightBackground}
         alt="background"
-        className="w-[30%] max-w-[30%] min-w-[200px] h-[100vh] absolute z-[-1] right-[-40px] opacity-50 sm:opacity-100"
+        className="w-[30%] max-w-[30%] min-w-[200px] h-[100vh] absolute z-[-1] right-[-40px] hidden sm:inline"
       />
 
       <div className="flex flex-col items-center justify-center gap-[38px] w-[100vw] h-[100vh] text-center whitespace-pre">
-        <p className="text-[#000000] text-[48px] font-bold ">
+        <p className="text-[#000000] [font-size:_clamp(2rem,5vw,4rem)] font-bold ">
           {'덮집회의로\n 정산을 쉽고, 빠르게,\n 그리고 차분하게'}
         </p>
-        <p className="text-[24px] font-[500]">
+        <p className="[font-size:_clamp(1rem,2vw,3rem)] font-[500]">
           {
             '덮집회의 웹서비스의 도움으로 정산의 고통을 줄이고,\n 친구들과 싸우는 걸 방지하여\n 부디 멋진 여행을 끝마쳐보도록 합시다.'
           }
@@ -34,7 +34,9 @@ function HomePage() {
           onClick={() => {
             navigate(routePath.setTitle);
           }}
-          className={'bg-primary-dark w-[300px] text-[16px] font-semibold'}
+          className={
+            'bg-primary-dark [width:_clamp(20rem,15vw,50rem)]  text-[16px] font-semibold'
+          }
         />
       </div>
     </div>
