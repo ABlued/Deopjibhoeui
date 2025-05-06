33825063dcc3fce4988f04f8bd169b35831b3b74
ABlued <skwodhkswkd@naver.com>
2025-05-06 21:14:07 +0900

    feat: 모바일 삭제 기능 추가

--- src/modules/result/mobile/components/DeleteButton.tsx
+++ src/modules/result/mobile/components/DeleteButton.tsx
@@ -1,7 +1,16 @@
 import { VscTrash } from 'react-icons/vsc';
+import { useHistoryStore } from '../../hooks/useHistory';
 
-function DeleteButton() {
-  return <VscTrash className="text-error-main" />;
+function DeleteButton({ id }: { id: string }) {
+  const { deleteHistoriesByID } = useHistoryStore();
+  return (
+    <VscTrash
+      className="text-error-main"
+      onClick={() => {
+        deleteHistoriesByID(id);
+      }}
+    />
+  );
 }
 
 export default DeleteButton;
--- src/modules/result/mobile/components/HistoryManagement.tsx
+++ src/modules/result/mobile/components/HistoryManagement.tsx
@@ -5,8 +5,8 @@ import EditButton from './EditButton';
 function HistoryManagement({ history }: { history: History }) {
   return (
     <div className="flex gap-3 relative top-0 right-0">
-      <DeleteButton />
       <EditButton history={history} />
+      <DeleteButton id={history.id} />
     </div>
   );
 }
